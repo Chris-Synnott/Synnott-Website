ec76c30f173df0174e7d046d62b62f2cd3dc1b81
Chris Synnott <csynnott@ucsd.edu>
2019-03-29 21:23:07 -0700

    fixed base projects js to load from array properly

--- main.js
+++ main.js
@@ -2,12 +2,12 @@
 
 var obj = {
     'projects': [{
-            "project_name": "pa1",
+            "project_title": "pa1",
             "type": "ai",
             "description": "pa1 description",
             "url": "2",
         }, {
-            "project_name": "pa2",
+            "project_title": "pa2",
             "type": "os",
             "description": "pa2 description",
             "url": "2"
@@ -59,10 +59,14 @@ window.onload = (function () {
 
 function displayProjects(array){
     console.log("Calling displayProjects")
+    topProject = `<div class="col-xs-12 col-sm-6 col-md-3 col-lg-3 mobile-no-padding"><a href="#"><div class="card"> <div class="card-header"><h2>`;
+    midProject = `</h2></div><div class="card-body"><p>`
+    botProject = `</p></div> <div class="card-bottom"><a href="#" rel="noopener">View</a></div></div> </a> </div>`
     code = ""
     for (i = 0; i < array.length; i++){
         console.log(array[i])
-        code += array[i].description;
+        newProject = topProject + array[i].project_title + midProject + array[i].description + botProject;
+        code += newProject;
     }
     document.getElementById("projects").innerHTML = ''; 
     document.getElementById("projects").innerHTML = code;
